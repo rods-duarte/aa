ca9dbdf95f140621b7bf91f2d056bc522aadd36d
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2023-07-23 23:19:57 -0300

    fix: mythril sword damage

--- src/main/java/net/rodsduarte/mythrilore/item/ModItems.java
+++ src/main/java/net/rodsduarte/mythrilore/item/ModItems.java
@@ -17,7 +17,7 @@ public class ModItems {
 
     public static final RegistryObject<Item> MYTHRIL_INGOT = ITEMS.register("mythril_ingot", () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> MYTHRIL_SWORD = ITEMS.register("mythril_sword", () -> new SwordItem(ModTiers.MYTHRIL, 4, -2.4F, new Item.Properties()));
+    public static final RegistryObject<Item> MYTHRIL_SWORD = ITEMS.register("mythril_sword", () -> new SwordItem(ModTiers.MYTHRIL, 6, -2.4F, new Item.Properties()));
 
     public static final RegistryObject<Item> MYTHRIL_PICKAXE = ITEMS.register("mythril_pickaxe", () -> new PickaxeItem(ModTiers.MYTHRIL, 1, -2.8F, new Item.Properties()));
 
